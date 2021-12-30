db5496229d2d2994f59d7f865761393632637d34
Prajwol Devkota <prajwol.devkota016@gmail.com>
2021-12-30 05:26:34 +0100

    added age section also used single useState to maintian all the value from <input /> tag. Used handleChange and handleSubmit dynamically to update the value

--- src/Form.js
+++ src/Form.js
@@ -1,25 +1,45 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  const [person, setPerson] = useState({
+    name: "",
+    email: "",
+    age: "",
+  });
   const [people, setPeople] = useState([]);
 
-  const handleClick = (e) => {
+  // const handleClick = (e) => {
+  //   e.preventDefault();
+  //   if (name && email) {
+  //     const person = {
+  //       id: new Date().getTime.toString(),
+  //       name: name,
+  //       email: email,
+  //     };
+  //     setPeople((people) => {
+  //       return [...people, person];
+  //     });
+  //     setName("");
+  //     setEmail("");
+  //   } else {
+  //     console.log("no inputs yet");
+  //   }
+  // };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setPerson({ ...person, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (name && email) {
-      const person = {
-        id: new Date().getTime.toString(),
-        name: name,
-        email: email,
-      };
-      setPeople((people) => {
-        return [...people, person];
-      });
-      setName("");
-      setEmail("");
-    } else {
-      console.log("no inputs yet");
+    if (person.name && person.email && person.age) {
+      const newPerson = { ...person, id: new Date().getTime().toString() };
+      setPeople([...people, newPerson]);
+      setPerson({ name: "", email: "", age: "" });
     }
   };
 
@@ -32,8 +52,8 @@ function Form() {
             type="text"
             id="name"
             name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={person.name}
+            onChange={handleChange}
           />
         </div>
         <div className="form-control">
@@ -42,20 +62,31 @@ function Form() {
             type="text"
             id="email"
             name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={person.email}
+            onChange={handleChange}
           />
         </div>
-        <button type="submit" onClick={handleClick}>
+        <div className="form-control">
+          <label htmlFor="age">Age: </label>
+          <input
+            type="text"
+            id="age"
+            name="age"
+            value={person.age}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" onClick={handleSubmit}>
           add person
         </button>
       </form>
       {people.map((person) => {
-        const { id, name, email } = person;
+        const { id, name, email, age } = person;
         return (
           <div className="item" key={id}>
-            <h4>{name}</h4>
-            <p>{email}</p>
+            <h4>Name: {name}</h4>
+            <p>Email: {email}</p>
+            <p>Age: {age}</p>
           </div>
         );
       })}
